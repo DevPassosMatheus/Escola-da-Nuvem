a32dabdbe48b90b082335427fc15a5a59412bc33
Matheus Passos <125465372+DevPassosMatheus@users.noreply.github.com>
2023-06-27 23:19:46 -0300

    Finalizando o projeto.
    Melhorando o layout, preenchendo os conteúdos e adicionando animações.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,31 @@
 import { LinkedinLogo, FileArrowDown } from "@phosphor-icons/react";
 
 interface CardProps {
-nome?: string
-sobre?: string
-imagem?: string
+  nome?: string;
+  sobre: string;
+  imagem: string;
+  CV?: string;
+  link?: string;
 }
 
-export const Card = ({sobre, nome}: CardProps) => {
- 
-return ( 
- <div className="flex flex-col rounded-[2rem] text-center justify-evenly px-[1.6rem] bg-zinc-800 border-zinc-700 text-white max-w-[36.2rem] min-[1024px]:min-w-[26.5rem] min-[1024px]:max-w-[29rem] min-[1280px]:min-w-[36.2rem] py-[2rem]">
-        <div className="flex flex-col items-center">
-            
-            <img src="/teste.jpg" className="max-w-[50%] rounded-full"/>
-            <h1 className="text-[2.4rem] py-[1.5rem]">{nome}</h1>
-        </div>
-        <p className="m-auto text-center text-[2rem] py-[1.8rem] px-[1.5rem]">{sobre}</p>
-        <div className="flex justify-center py-[1.5rem]">
-            <LinkedinLogo size="48" />
-            <FileArrowDown  size="48" />
-        </div>
-        <a className="justify-self-end py-3 px-3 text-[2rem]">Saiba mais</a>
- </div> 
+export const Card = ({ sobre, nome, CV, imagem, link }: CardProps) => {
+  
 
-
-
-
-)
+  return (
+    <div className="flex flex-col rounded-[2rem] text-center justify-evenly px-[1.6rem] shadow-2xl bg-[#26303F] border-zinc-700 py-[2rem] max-w-[40rem]">
+      <div className="flex flex-col items-center">
+        <img src={imagem} className="max-w-[50%] rounded-full" />
+        <h1 className="text-[#FF9900] text-[2.4rem] py-[1.5rem] font-bold">{nome}</h1>
+      </div>
+      <p className="m-auto text-center text-[2rem] py-[1.8rem] px-[1.5rem] text-[#C5D7FF]">{sobre}</p>
+      <div className="flex justify-center py-[1.5rem] text-[#FF9900]">
+        <a href={link} target="_blank">
+          <LinkedinLogo size="48" />
+        </a>
+        <a href={CV} download>
+          <FileArrowDown size="48" />
+        </a>
+      </div>
+    </div>
+  );
 };
-
- 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ module.exports = {
   content: ["./src/**/*.tsx", "./public/index.html"],
   theme: {
     extend: {
-      backgroundImage: {
-      'nuvem' : "linear-gradient(to right, #011633, #142c50, #27436e, #3b5c8e, #4e77af)"
+      fontFamily: {
+         
+        'Nunito': ['Nunito', 'sans-serif'],
       },
 
 
